a158ae6c5e0bc9d6eb0878b65fa16a1445d771c2
garretgonzales <garret915@gmail.com>
2020-06-20 19:29:22 -0500

    Updates

---
+++ precache-manifest.3ba8c1a222c77261019803c5728c2925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d6f6d2e2d61451dbe7cfc3cfcda98b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "9073ee1181ed83592d02",
+    "url": "/react-todo/static/css/main.64440f92.chunk.css"
+  },
+  {
+    "revision": "11d08c67136a006457c5",
+    "url": "/react-todo/static/js/2.7537ceea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.7537ceea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9073ee1181ed83592d02",
+    "url": "/react-todo/static/js/main.7d8a31ee.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
